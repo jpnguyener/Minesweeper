0eddcb948c6e1f4a8672e27aebed84f39d5a9729
jpnguyener <jpnguyener@yahoo.com>
2018-08-24 01:23:50 -0400

    finished minesweeper game (forgot to make regular commits/pushes lol)

--- minesweeper.js
+++ minesweeper.js
@@ -10,21 +10,67 @@ var grid;
 var cols;
 var rows;
 var w = 20;
+var totalBombs = 10;
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(201, 201);
   cols = floor(width / w);
   rows = floor(height / w);
   grid = make2DArray(20, 20)
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
-      grid[i][j] = new Cell(i * w, j * w);
+      grid[i][j] = new Cell(i, j, w);
+    }
+  }
+
+  var options = [];
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      options.push([i, j]);
+
+    }
+  }
+
+  for (var n = 0; n <totalBombs; n++) {
+    var index = floor(random(options.length));
+    var choice = options[index];
+    var i = choice[0];
+    var j = choice[1];
+    options.splice(index, 1);
+    grid[i][j].bomb = true;
+  }
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].countBombs();
+    }
+  }
+}
+
+function gameOver() {
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].revealed = true;
+    }
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      if (grid[i][j].contains(mouseX, mouseY)) {
+        grid[i][j].reveal();
+
+        if (grid[i][j].bomb) {
+          gameOver();
+        }
+      }
     }
   }
 }
 
 function draw() {
-  background(0);
+  background(255);
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
       grid[i][j].show();
